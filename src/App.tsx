f561d956f36eda5561c0d4239b13bed199d9242c
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-02-13 21:46:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ReactXnft, { AnchorDom, View, Text } from "react-xnft";
+
+function App() {
+  return (
+    <View>
+      <Text style={{ color: 'blue' }}>Hello, World!</Text>
+    </View>
+  );
+}
+
+export default App;
